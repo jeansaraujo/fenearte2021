a42fd1fdf42685a42cc69aa96f28ccbe0e51fbee
JEANELDER <JEANELDER@HOTMAIL.COM>
2021-11-23 00:23:12 -0300

    retirado url mapa

--- src/app/menu/menu.page.ts
+++ src/app/menu/menu.page.ts
@@ -1,4 +1,4 @@
-import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser/ngx';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,12 +9,22 @@ import { Component, OnInit } from '@angular/core';
 
 export class MenuPage implements OnInit {
 
-  constructor(private iab: InAppBrowser) {
+  options: InAppBrowserOptions = {
+    location : 'no',//Or 'no'
+    hidden : 'no', //Or  'yes'
+    zoom : 'yes',//Android only ,shows browser zoom controls
+    hideurlbar:'yes',//Or 'no'
+    fullscreen:'yes',
+  };
+
+  constructor(private iab: InAppBrowser ) {
 
   }
 
+
   goMapa(): void{
-    const browser = this.iab.create('https://jeansaraujo.github.io/apis/2021/mapa-fenearte/index.html');
+    //,"hideurlbar:'yes'"
+    const browser = this.iab.create('https://jeansaraujo.github.io/apis/2021/mapa-fenearte/index.html','_self',this.options);
     //browser.executeScript();
     //browser.insertCSS();
     //browser.on('loadstop').subscribe(event => {
@@ -22,6 +32,9 @@ export class MenuPage implements OnInit {
     //});
     // browser.close();
     // alert("Ajustar o InAppBrowser >> https://jeansaraujo.github.io/apis/2021/mapa-fenearte/index.html");
+  }
+  goGMaps(): void{
+    const browser = this.iab.create('https://goo.gl/maps/VWkDNpfDo1AedsccA','_blank',this.options);
   }
   ngOnInit() {
   }
